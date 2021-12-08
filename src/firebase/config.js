4eafdfd071a41bddc6d1bf18b3869c5cfe990318
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-08 20:11:44 +0530

    in progress

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBx7mqK_J5NQCtRRxwyJ0KVGAwfZvZXQt8",
+    authDomain: "mymoney-financial-tracker.firebaseapp.com",
+    projectId: "mymoney-financial-tracker",
+    storageBucket: "mymoney-financial-tracker.appspot.com",
+    messagingSenderId: "358150442997",
+    appId: "1:358150442997:web:314334422361feabb3dabc"
+  };
+
+  // Init Firebase
+  firebase.initializeApp(firebaseConfig)
+
+  // Init Service
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  export { projectFirestore, projectAuth }
---
+++ src/hooks/useSignup.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { projectAuth } from '../firebase/config'
+
+
+export const useSignup = () => {
+    const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+
+    const signup = async (email, password, displayName) => {
+        setError(null)
+        setIsPending(true)
+
+        try {
+            // Signup user
+            const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+            console.log(res.user);
+            
+            if(!res) {
+                throw new Error('Could not complete signup')
+            }
+
+            // Add Display Name to user
+            await res.user.updateProfile({ displayName })
+
+            setIsPending(false)
+            setError(null)
+
+        }
+        catch (err) {
+            console.log(err.message);
+            setError(err.message)
+            setIsPending(false)
+        }
+    }
+
+    return { error, isPending, signup }
+
+}
