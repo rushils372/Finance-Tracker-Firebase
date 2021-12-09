7fcffc3f55ab6a0ff9bef3fb8be361546ab58d60
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-09 18:05:03 +0530

    in development...

---
+++ src/context/AuthContext.js
@@ -0,0 +1,25 @@
+import { createContext, useReducer } from "react";
+
+export const AuthContext = createContext()
+
+export const authReducer =(state, action) => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+
+    dispatch({ type: 'LOG_IN'})
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch}}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
